71f7cfc93297b19320d70948f4727774d4c53e3d
Yuki MIZUNO <mizuyuu0904@gmail.com>
2016-07-09 17:16:07 +0900

    Change navigation drawer item position settings

--- Harekaze/Views/NavigationDrawerMaterialTableViewCell.swift
+++ Harekaze/Views/NavigationDrawerMaterialTableViewCell.swift
@@ -11,85 +11,21 @@ import Material
 
 class NavigationDrawerMaterialTableViewCell: MaterialTableViewCell {
 
-	// Change navigation icon and label position
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		imageView?.frame = CGRect(x: 16, y: 12, width: 24, height: 24)
+		textLabel?.frame = CGRect(x: 72, y: 8, width: self.frame.width - 16, height: 32)
+	}
+
 	internal override func prepareView() {
 		super.prepareView()
-		imageView?.translatesAutoresizingMaskIntoConstraints = false
-		textLabel?.translatesAutoresizingMaskIntoConstraints = false
 
-		self.addConstraints([
-			NSLayoutConstraint(
-				item: imageView!,
-				attribute: .Top,
-				relatedBy: .Equal,
-				toItem: self,
-				attribute: .Top,
-				multiplier: 1.0,
-				constant: 12
-			),
-			// IMPORTANT: Material design guideline: left = 16px
-			NSLayoutConstraint(
-				item: imageView!,
-				attribute: .Left,
-				relatedBy: .Equal,
-				toItem: self,
-				attribute: .Left,
-				multiplier: 1.0,
-				constant: 16
-			),
-			NSLayoutConstraint(
-				item: imageView!,
-				attribute: .Height,
-				relatedBy: .Equal,
-				toItem: nil,
-				attribute: .Height,
-				multiplier: 1.0,
-				constant: 24
-			),
-			NSLayoutConstraint(
-				item: imageView!,
-				attribute: .Width,
-				relatedBy: .Equal,
-				toItem: nil,
-				attribute: .Width,
-				multiplier: 1.0,
-				constant: 24
-			)
-			]
-		)
+		// Change navigation icon and label position
 
-
-		self.addConstraints([
-			NSLayoutConstraint(
-				item: textLabel!,
-				attribute: .Top,
-				relatedBy: .Equal,
-				toItem: self,
-				attribute: .Top,
-				multiplier: 1.0,
-				constant: 8
-			),
-			// IMPORTANT: Material design guideline: left = 72px
-			NSLayoutConstraint(
-				item: textLabel!,
-				attribute: .Left,
-				relatedBy: .Equal,
-				toItem: self,
-				attribute: .Left,
-				multiplier: 1.0,
-				constant: 72
-			),
-			NSLayoutConstraint(
-				item: textLabel!,
-				attribute: .Height,
-				relatedBy: .Equal,
-				toItem: nil,
-				attribute: .Height,
-				multiplier: 1.0,
-				constant: 32
-			)
-			]
-		)
+		// IMPORTANT: Material design guideline: left = 16px
+		imageView?.frame = CGRect(x: 16, y: 12, width: 24, height: 24)
+		// IMPORTANT: Material design guideline: left = 72px
+		textLabel?.frame = CGRect(x: 72, y: 8, width: self.frame.width - 16, height: 32)
 
 		// Set icon and label color
 		textLabel!.textColor = MaterialColor.grey.darken4
