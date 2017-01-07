db2bc2afd2d8826a4e655259106de38ea867919b
Yuki MIZUNO <mizuyuu0904@gmail.com>
2017-01-08 02:19:28 +0900

    Update copyright year

--- Harekaze/ViewControllers/Downloads/DownloadsTableViewController.swift
+++ Harekaze/ViewControllers/Downloads/DownloadsTableViewController.swift
@@ -4,7 +4,7 @@
 * Harekaze
 * Created by Yuki MIZUNO on 2016/08/21.
 *
-* Copyright (c) 2016, Yuki MIZUNO
+* Copyright (c) 2016-2017, Yuki MIZUNO
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
